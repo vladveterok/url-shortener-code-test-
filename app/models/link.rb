adb058f388f9c953f2b8227f58df424130d17f3b
vladveterok <vl.berezhnoi@gmail.com>
2021-01-25 12:51:41 +0200

    Move validation messages from link model to localization. Move url length constrains from link model to lib/constants

--- app/models/link.rb
+++ app/models/link.rb
@@ -4,9 +4,9 @@ class Link
   attr_accessor :url, :slug
 
   validates! :url, presence: true, length: {
-    minimum: 3,
-    maximum: 2048,
-    too_short: 'Too short!',
-    too_long: 'Too long!'
+    minimum: Constants::URL_LENGTH.min,
+    maximum: Constants::URL_LENGTH.max,
+    too_short: I18n.t('url.messages.too_short'),
+    too_long: I18n.t('url.messages.too_long')
   }
 end
---
+++ lib/constants.rb
@@ -0,0 +1,3 @@
+module Constants
+  URL_LENGTH = (3..2048).freeze
+end
